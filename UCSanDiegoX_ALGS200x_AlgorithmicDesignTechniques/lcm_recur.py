74d4d97fc9fa410bede4b87de5906959b925e7a5
BianLong0618 <bianlong0618@qq.com>
2021-08-19 09:38:09 +0800

    Added the lcm algorithm in UCSanDiegoX ALGS200x.

---
+++ UCSanDiegoX_ALGS200x_AlgorithmicDesignTechniques/lcm_recur.py
@@ -0,0 +1,40 @@
+# Uses python3
+import sys
+
+"""
+The recursive Greatest Common Divisor algorithm.
+Input @para: integers a and b.
+Output @para: the greatest common divisor of a and b.
+"""
+def gcd_recur( a, b ):
+
+    #  Check out which is the bigger one and which is the smaller one.
+    if a >= b:
+        smaller = b
+        bigger = a
+    else:
+        smaller = a
+        bigger = b
+
+    #  Base case,
+    #  when the bigger number is a multiply of the smaller number, gcd is the smaller one.
+    if smaller == 0:
+        return bigger
+
+    #  The recursive step
+    #  Assume d is the gcd of a & b, then,
+    #  bigger / d = ( k * smaller + reminder ) / d
+    #             =   k * smaller / d + reminder / d
+    #  therefore, gcd( bigger, smaller ) = gcd( smaller, reminder ).
+    return gcd_recur( bigger % smaller, smaller )
+
+
+def lcm_recur( a, b ):
+    gcd = gcd_recur( a, b )
+    return a * b // gcd
+
+
+if __name__ == "__main__":
+    input = sys.stdin.read()
+    a, b = map(int, input.split())
+    print(lcm_recur(a, b))
--- test.py
+++ test.py
@@ -1,37 +1,4 @@
-# Uses python3
 import sys
 
-"""
-The recursive Greatest Common Divisor algorithm.
-Input @para: integers a and b.
-Output @para: the greatest common divisor of a and b.
-"""
-def gcd_recur( a, b ):
-
-    #  Check out which is the bigger one and which is the smaller one.
-    if a >= b:
-        smaller = b
-        bigger = a
-    else:
-        smaller = a
-        bigger = b
-
-    #  Base case,
-    #  when the bigger number is a multiply of the smaller number, gcd is the smaller one.
-    if smaller == 0:
-        return bigger
-
-    #  The recursive step
-    #  Assume d is the gcd of a & b, then,
-    #  bigger / d = ( k * smaller + reminder ) / d
-    #             =   k * smaller / d + reminder / d
-    #  therefore, gcd( bigger, smaller ) = gcd( smaller, reminder ).
-    return gcd_recur( bigger % smaller, smaller )
-
-
-if __name__ == "__main__":
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(gcd_recur(a, b))
-
-
+input = sys.stdin.read()
+a, b = map(int, input.split())
