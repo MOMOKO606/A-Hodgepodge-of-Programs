bda24ff90782a349fcd7dfa3dd4ad2fdec6e44f9
BianLong0618 <bianlong0618@qq.com>
2021-11-24 23:27:45 +0800

    Update the iterative dynamic programming algorithm for the jump game(leetcode 55) problem.

--- Solution.py
+++ Solution.py
@@ -420,6 +420,17 @@ class Solution:
 
 
     #  Solution3: the iterative dynamic programming algorithm.
+    def canJump_iter(self, nums: List[int]) -> bool:
+        n = len(nums)
+        ans = [False] * n
+        ans[0] = True
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if i - j <= nums[j]:
+                    ans[i] = True
+                    break
+        return ans[n - 1]
+
 
 
 
@@ -470,4 +481,4 @@ if __name__ == "__main__":
     print(S.coinChange_iter([2], 3))  # Leetcode, 322
     print(S.uniquePaths(7, 3))  # Leetcode, 62
     print("--------------------------------")
-    print(S.canJump_recur_memo(l6))  # Leetcode, 55
+    print(S.canJump_iter(l7))  # Leetcode, 55
