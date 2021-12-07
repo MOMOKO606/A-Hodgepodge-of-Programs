76711e50339e40ad8012ac02715d7c28ef602d7e
BianLong0618 <bianlong0618@qq.com>
2021-12-07 18:44:57 +0800

    Update the optimized solution for the Merge Sorted Array(leetcode 88).

--- Solution.py
+++ Solution.py
@@ -1136,7 +1136,6 @@ class Solution:
     Input: list1 = [1,2,4], list2 = [1,3,4]
     Output: [1,1,2,3,4,4]
     """
-
     #  Solution01: the iterative version.
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = cur = ListNode()
@@ -1167,9 +1166,40 @@ class Solution:
             return list2
 
 
+    """
+    88. Merge Sorted Array(Easy)
+    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+    and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+    
+    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+    To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
+    and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+    
+    Example:
+    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    Output: [1,2,2,3,5,6]
+    Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+    The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+    """
+    def merge(self, nums1, m, nums2, n):
+        while m and n:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[ m + n - 1 ] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+        nums1[:n] = nums2[:n]
 
 
-    #  Drive code.
+
+
+
+
+
+#  Drive code.
 if __name__ == "__main__":
     #  Create an instance
     S = Solution()
@@ -1255,3 +1285,5 @@ if __name__ == "__main__":
     print(S.rotate01([-1, -100, 3, 99], 2))  # Leetcode 189
     print(linkedlist2Array(S.mergeTwoLists(linkedlist07, linkedlist08)))  # Leetcode 21
     print("--------------------------------")
+   # print(S.merge([1,2,3,0,0,0], 3, [2,5,6], 3))
+    print(S.merge([2,0], 1, [1], 1))
