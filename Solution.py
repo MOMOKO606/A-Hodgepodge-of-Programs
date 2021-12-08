8fb84b4f9c1976395a62f784dd6f7101d2a08a6a
BianLong0618 <bianlong0618@qq.com>
2021-12-08 13:18:29 +0800

    Update a iterative solution using for loop for the Plus one(leetcode 66).

--- Solution.py
+++ Solution.py
@@ -1209,7 +1209,7 @@ class Solution:
     Incrementing by one gives 123 + 1 = 124.
     Thus, the result should be [1,2,4].
     """
-
+    #  Solution01: the iterative version using while loop.
     def plusOne(self, digits: List[int]) -> List[int]:
 
         flag = 1
@@ -1226,6 +1226,25 @@ class Solution:
             digits = [1] + digits
 
         return digits
+
+
+    #  Solution02: the iterative version using for loop.
+    def plusOne_iter(self, digits: List[int] ) -> List[int]:
+        digits[-1] += 1
+        for i in reversed( range( 1, len(digits) ) ):
+            if digits[i] < 9:
+                break
+            digits[i] = 0
+            digits[i - 1] += 1
+
+        if digits[0] > 9:
+            digits[0] = 0
+            return [1] + digits
+        return digits
+
+
+
+
 
 
 
@@ -1321,4 +1340,5 @@ if __name__ == "__main__":
     print(linkedlist2Array(S.mergeTwoLists(linkedlist07, linkedlist08)))  # Leetcode 21
     print(S.merge([2, 0], 1, [1], 1))
     print("--------------------------------")
-    print(S.plusOne(digits04))
+ #   print(S.plusOne(digits04))  #  Leetcode 66
+    print(S.plusOne_iter(digits01))  # Leetcode 66
