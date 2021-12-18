c7d404f70e6b0756fa21073a639f925008be13a4
BianLong0618 <bianlong0618@qq.com>
2021-12-18 15:43:34 +0800

    Update the 2nd round code for the merge(Leetcode 88).

--- test.py
+++ test.py
@@ -30,6 +30,7 @@ def printLinkedlist(head: Optional[ListNode]) -> None:
 
 class Solution:
 
+    #  Leetcode 01
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
         for i, num in enumerate(nums):
@@ -37,6 +38,24 @@ class Solution:
             if key in map:
                 return [map[key], i]
             map[num] = i
+
+    #  Leetcode 02
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = cur = ListNode()
+        carry = 0
+        while l1 or l2 or carry:
+
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+
+            cur.next = ListNode(carry % 10)
+            cur = cur.next
+            carry //= 10
+        return dummy.next
 
     # def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
     #
@@ -64,23 +83,7 @@ class Solution:
     #     num2 = ll2ReverseNum(l2)
     #     return num2Reversell(num1 + num2)
 
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = cur = ListNode()
-        carry = 0
-        while l1 or l2 or carry:
-
-            if l1:
-                carry += l1.val
-                l1 = l1.next
-            if l2:
-                carry += l2.val
-                l2 = l2.next
-
-            cur.next = ListNode(carry % 10)
-            cur = cur.next
-            carry //= 10
-        return dummy.next
-
+    #  Leetcode 03
     def lengthOfLongestSubstring(self, s: str) -> int:
         l, max_count, usedchar = -1, 0, {}
         for r in range(len(s)):
@@ -91,6 +94,7 @@ class Solution:
             usedchar[s[r]] = r
         return max_count
 
+    #  Leetcode 11
     def maxArea(self, height: List[int]) -> int:
         area, i, j = 0, 0, len(height) - 1
         while i < j:
@@ -101,6 +105,7 @@ class Solution:
                 j -= 1
         return area
 
+    #  Leetcode 15
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         n = len(nums)
@@ -127,6 +132,17 @@ class Solution:
                     r -= 1
         return ans
 
+    #  Leetcode 21
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
     # def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
     #     dummy = cur = ListNode()
     #     while l1 and l2:
@@ -141,16 +157,7 @@ class Solution:
     #     cur.next = l1 or l2
     #     return dummy.next
 
-    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if not l1 or not l2:
-            return l1 or l2
-        if l1.val < l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
-        else:
-            l2.next = self.mergeTwoLists(l1, l2.next)
-            return l2
-
+    #  Leetcode 26
     def removeDuplicates(self, nums: List[int]) -> int:
         j = -1
         for i in range(len(nums)):
@@ -159,6 +166,17 @@ class Solution:
                 nums[j] = nums[i]
         return j + 1
 
+    #  Leetcode 55
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        ans = [False] * n
+        ans[0] = True
+        for i in range(1, n):
+            for j in range(i):
+                if i - j <= nums[j] and ans[j] is True:
+                    ans[i] = True
+                    break
+        return ans[n - 1]
 
     # def canJump(self, nums: List[int]) -> bool:
     #     n = len(nums)
@@ -169,7 +187,6 @@ class Solution:
     #         if reach >= n - 1:
     #             return True
     #         reach = max(reach, j + nums[j])
-
 
     # def canJump(self, nums:List[int]) -> bool:
     #
@@ -191,18 +208,13 @@ class Solution:
     #     memo[0] = True
     #     return _canJump( nums, memo)
 
-
-    def canJump(self, nums:List[int]) -> bool:
-        n = len(nums)
-        ans = [False] * n
-        ans[0] = True
-        for i in range(1, n):
-            for j in range(i):
-                if i - j <= nums[j] and ans[j] is True:
-                    ans[i] = True
-                    break
-        return ans[n - 1]
-
+    #  Leetcode 62
+    def uniquePaths(self, m: int, n: int) -> int:
+        ans = [[1 for j in range(n)] for i in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                ans[i][j] = ans[i - 1][j] + ans[i][j - 1]
+        return ans[m - 1][n - 1]
 
     #  Recursive version with a memo.
     # def uniquePaths(self, m: int, n:int) -> int:
@@ -220,23 +232,97 @@ class Solution:
     #     memo = [[0 for j in range(n)] for i in range(m)]
     #     return _uniquePaths(m, n, memo)
 
-
-    def uniquePaths(self, m: int, n: int) -> int:
-        ans = [[1 for j in range(n)] for i in range(m)]
-        for i in range(1, m):
-            for j in range(1, n):
-                ans[i][j] = ans[i - 1][j] + ans[i][j - 1]
-        return ans[m - 1][n - 1]
-
-
     #  Leetcode 66
     def plusOne(self, digits: List[int]) -> List[int]:
-        for j in reversed(range(len(digits))):
-            if digits[j] + 1 < 10:
-                digits[j] += 1
-                return digits
-            digits[j] = 0
-        return [1] + digits
+
+        if len(digits) < 1:
+            return [1]
+
+        if digits[-1] + 1 < 10:
+            digits[-1] += 1
+            return digits
+
+        return self.plusOne(digits[:-1]) + [0]
+
+    # def plusOne(self, digits: List[int]) -> List[int]:
+    #     for j in reversed(range(len(digits))):
+    #         if digits[j] + 1 < 10:
+    #             digits[j] += 1
+    #             return digits
+    #         digits[j] = 0
+    #     return [1] + digits
+
+    #  Leetcode 70.
+    def climbStairs(self, n: int) -> int:
+        f1 = 1
+        f2 = 2
+        if n == 1: return f1
+        if n == 2: return f2
+        for i in range(3, n + 1):
+            f3 = f1 + f2
+            f1 = f2
+            f2 = f3
+        return f3
+
+    #  The recursive algorithm with a memo.
+    # def climbStairs(self, n: int) -> int:
+    #
+    #     def _climbStairs( n: int, memo: List[int]) -> int:
+    #         if memo[n]:
+    #             return memo[n]
+    #         if n == 1:
+    #             memo[n] = 1
+    #             return 1
+    #         if n == 2:
+    #             memo[n] = 2
+    #             return 2
+    #         return _climbStairs( n - 1, memo ) + _climbStairs( n - 2, memo )
+    #
+    #     memo= [0] * (n + 1)
+    #     return _climbStairs(n, memo)
+
+    #  Leetcode 80
+    def removeDuplicates02(self, nums: List[int]) -> int:
+        k = -1
+        for j in range(len(nums)):
+            if j < 2 or nums[j] > nums[k - 1]:
+                k += 1
+                nums[k] = nums[j]
+        return k + 1
+
+    #  Leetcode 82
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = dummy = ListNode()
+        cur = dummy.next = head
+        while cur:
+            while cur.next and cur.val == cur.next.val:
+                cur = cur.next
+            if prev.next != cur:
+                prev.next = cur.next
+            else:
+                prev = cur
+            cur = cur.next
+        return dummy.next
+
+
+    #  Leetcode 83
+    def deleteDuplicates03(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        while cur:
+            while cur.next and cur.val == cur.next.val:
+                cur.next = cur.next.next
+            cur = cur.next
+        return head
+
+    #  Leetcode 88
+    def merge(self, nums01: List[int], nums02: List[int], m: int, n: int) -> None:
+        while m and n:
+            if nums01[m - 1] > nums02[n - 1]:
+                nums01[ m + n - 1] = nums01[m - 1]
+                m -= 1
+            else:
+                nums01[m + n - 1] = nums02[n - 1]
+        nums01[:n] = nums02[:n]
 
 
 
@@ -268,13 +354,42 @@ if __name__ == "__main__":
     l2 = array2Linkedlist([1, 3, 4])
     printLinkedlist(S.mergeTwoLists(l1, l2))
 
+    #  Leetcode 26
+    print(S.removeDuplicates([1, 1, 1, 2, 2, 3]))
+
     #  Leetcode 55
-    print(S.canJump([2,3,1,1,4]))
+    print(S.canJump([2, 3, 1, 1, 4]))
 
-    #  Leetcode 42
-    print( S.uniquePaths(3, 7))
+    #  Leetcode 62
+    print(S.uniquePaths(3, 7))
 
-
-    print("---------------------------------")
     #  Leetcode 66
-    print(S.plusOne([9]))
+    print(S.plusOne([1, 2, 3, 9, 9]))
+
+    #  Leetcode 70
+    print(S.climbStairs(3))
+
+    #  Leetcode 80
+    print(S.removeDuplicates02([0, 0, 1, 1, 1, 1, 2, 3, 3]))
+
+    #  Leetcode 82
+    l1 = array2Linkedlist([1, 2, 3, 3, 4, 4, 5])
+    l2 = array2Linkedlist([1, 1, 1, 2, 3])
+    printLinkedlist(S.deleteDuplicates(l1))
+    printLinkedlist(S.deleteDuplicates(l2))
+
+    #  Leetcode 83
+    l1 = array2Linkedlist([1, 1, 2])
+    l2 = array2Linkedlist([1, 1, 2, 3, 3])
+    printLinkedlist(S.deleteDuplicates03(l1))
+    printLinkedlist(S.deleteDuplicates03(l2))
+
+    #  leetcode 88
+    print("---------------------------------")
+    nums01,nums02 = [1,2,3,0,0,0], [2,5,6]
+    S.merge(nums01, nums02, 3, 3)
+    print(nums01)
+
+    nums01, nums02 = [0], [1]
+    S.merge(nums01, nums02, 0, 1)
+    print(nums01)
