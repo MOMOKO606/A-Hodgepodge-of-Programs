41234aeab83743de50c3feff15b2fe0d87b27e56
BianLong0618 <bianlong0618@qq.com>
2022-01-05 18:00:50 +0800

    Update the solutions for the postorder(590. N-ary Tree Postorder Traversal).

--- Solution.py
+++ Solution.py
@@ -1916,6 +1916,43 @@ class Solution:
     #     return ans
 
 
+    """
+    590. N-ary Tree Postorder Traversal( Easy )
+    Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+    Nary-Tree input serialization is represented in their level order traversal. 
+    Each group of children is separated by the null value (See examples)
+    
+    Example:
+    Input: root = [1,null,3,2,4,null,5,6]
+    Output: [5,6,3,2,4,1]
+    """
+    #  The recursive version.
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        ans = []
+        for child in root.children:
+            ans += self.postorder( child )
+        ans += [root.val]
+        return ans
+
+
+    # #  The iterative version.
+    # def postorder(self, root: 'Node') -> List[int]:
+    #     ans, stack = [], [root]
+    #     while stack:
+    #         root = stack.pop()
+    #         if root:
+    #             ans += [root.val]
+    #             for child in root.children:
+    #                 stack.append( child )
+    #     return ans[::-1]
+
+
+
+
+
+
 
 
 
