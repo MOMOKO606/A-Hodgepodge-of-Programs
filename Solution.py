d1019f91dd8e9e0eb12e887c21753236aa002641
BianLong0618 <bianlong0618@qq.com>
2022-01-20 11:54:43 +0800

    Update the greedy solution for the findContentChildren (Leetcode 455).

--- Solution.py
+++ Solution.py
@@ -3143,6 +3143,29 @@ class Solution:
         # return ans
 
 
+    """
+    455. Assign Cookies (Easy)
+    https://leetcode.com/problems/assign-cookies/description/
+    
+    Example:
+    Input: g = [1,2,3], s = [1,1]
+    Output: 1
+    Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
+    And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+    You need to output 1.
+    """
+    #  The greedy solution.
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i, j, m, n = 0, 0, len(g), len(s)
+        while i < m and j < n:
+            if g[i] <= s[j]: i += 1
+            j += 1
+        return i
+
+
+
 #  Drive code.
 if __name__ == "__main__":
     #  Create an instance
@@ -3425,11 +3448,17 @@ if __name__ == "__main__":
                          ["E", "E", "E", "E", "E", "E", "E", "E"], ["E", "E", "M", "M", "E", "E", "E", "E"]]
                         , [0, 0]))
 
-    print("---------------------------------------------------------------")
+
     #  Leetcode 126
     print(S.findLadders("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
     print(S.findLadders("hit", "cog", ["hot", "dot", "dog", "lot", "log"]))
     print(S.findLadders("a", "c", ["a", "b", "c"]))
+
+    #  Leetcode 455
+    print("---------------------------------------------------------------")
+    print(S.findContentChildren([1,2,3],[1,1]))
+    print(S.findContentChildren([1,2], [1,2,3]))
+
 
 """
 ..................佛祖开光 ,永无BUG...................
