afb93259d1099212aad79a1840bbf0c734fb5933
BianLong0618 <bianlong0618@qq.com>
2022-01-30 19:39:14 +0800

    Update the solution for the Best Time to Buy and Sell Stock III (Leetcode 123).

--- Solution.py
+++ Solution.py
@@ -3870,12 +3870,45 @@ class Solution:
     Output: 0
     Explanation: In this case, no transactions are done and the max profit = 0.
     """
+
     def maxProfit121(self, prices: List[int]) -> int:
         curMin, ans = math.inf, 0
         for num in prices:
             curMin = min(curMin, num)
             ans = max(ans, num - curMin)
         return ans
+
+    """
+    123. Best Time to Buy and Sell Stock III ( Hard )
+    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+    
+    Example 01:
+    Input: prices = [3,3,5,0,0,3,1,4]
+    Output: 6
+    Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+    Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+    
+    Example 02:
+    Input: prices = [1,2,3,4,5]
+    Output: 4
+    Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+    Note that you cannot buy on day 1, buy on day 2 and sell them later, 
+    as you are engaging multiple transactions at the same time. You must sell before buying again.
+    
+    Example 03:
+    Input: prices = [7,6,4,3,1]
+    Output: 0
+    Explanation: In this case, no transaction is done, i.e. max profit = 0.
+    """
+    def maxProfit123(self, prices: List[int]) -> int:
+        if not prices: return 0
+        s1, s2, s3, s4 = -math.inf, -math.inf, -math.inf, -math.inf
+        for price in prices:
+            s1 = max( s1, -price)
+            s2 = max( s2, s1 + price)
+            s3 = max( s3, s2 - price)
+            s4 = max( s4, s3 + price)
+        return s4
 
 
 #  Drive code.
@@ -4280,10 +4313,15 @@ if __name__ == "__main__":
     print(S.mostPoints([[3, 2], [4, 3], [4, 4], [2, 5]]))
     print(S.mostPoints([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]))
 
-    print("-------------------------------------------------------")
     #  Leetcode 121
     print(S.maxProfit121([7, 1, 5, 3, 6, 4]))
     print(S.maxProfit121([7, 6, 4, 3, 1]))
+
+    print("-------------------------------------------------------")
+    #  Leetcode 123
+    print(S.maxProfit123([3, 3, 5, 0, 0, 3, 1, 4]))
+    print(S.maxProfit123([1, 2, 3, 4, 5]))
+    print(S.maxProfit123([7, 6, 4, 3, 1]))
 
 """
 ..................佛祖开光 ,永无BUG...................
