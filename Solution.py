a4a3d166e8fa3a67cc501a11e607629db5e1c98b
BianLong0618 <bianlong0618@qq.com>
2022-02-04 19:44:51 +0800

    Update the iterative dp solution for the minDistance(Leetcode 72).

--- Solution.py
+++ Solution.py
@@ -4172,23 +4172,39 @@ class Solution:
     exention -> exection (replace 'n' with 'c')
     exection -> execution (insert 'u')
     """
-    #  The recursive dp solution with a memo.
+    # #  The recursive dp solution with a memo.
+    # def minDistance(self, word1: str, word2: str) -> int:
+    #     @cache
+    #     def _minDistance( i, j ):
+    #         #  Base case
+    #         if i < 0 and j < 0: return 0
+    #         if i < 0 and j >= 0: return j + 1
+    #         if i >= 0 and j < 0: return i + 1
+    #         if word1[i] == word2[j]:
+    #             return _minDistance(i - 1, j - 1)
+    #         else:
+    #             remove = _minDistance(i - 1, j)
+    #             #  The delete of word2 equals the insert of word1.
+    #             insert = _minDistance(i, j - 1)
+    #             replace = _minDistance(i - 1, j - 1)
+    #             return min(remove, insert, replace) + 1
+    #     return _minDistance(len(word1) - 1, len(word2) - 1)
+
+    #  The iterative dp solution.
     def minDistance(self, word1: str, word2: str) -> int:
-        @cache
-        def _minDistance( i, j ):
-            #  Base case
-            if i < 0 and j < 0: return 0
-            if i < 0 and j >= 0: return j + 1
-            if i >= 0 and j < 0: return i + 1
-            if word1[i] == word2[j]:
-                return _minDistance(i - 1, j - 1)
-            else:
-                remove = _minDistance(i - 1, j)
-                #  The delete of word2 equals the insert of word1.
-                insert = _minDistance(i, j - 1)
-                replace = _minDistance(i - 1, j - 1)
-                return min(remove, insert, replace) + 1
-        return _minDistance(len(word1) - 1, len(word2) - 1)
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1): dp[i][0] = i
+        for j in range(n + 1): dp[0][j] = j
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+        return dp[-1][-1]
+
+
 
 
 
