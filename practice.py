8c1eef4629ee0d77e7545a57e3a6abe888bb0cf6
BianLong0618 <bianlong0618@qq.com>
2022-07-07 16:45:47 -0400

    Update a 3-line solution for 125.

--- practice.py
+++ practice.py
@@ -1,5 +1,5 @@
 class Solution:
-    #  1137 (easy)
+    #  1137.N-th Tribonacci Number(easy)
     def tribonacci(self, n: int) -> int:
         if n < 2: return n
         if n == 2: return 1
@@ -8,9 +8,23 @@ class Solution:
             f0, f1, f2 = f1, f2, f0 + f1 + f2
         return f2
 
+    #  125.Valid Palindrome(easy)
+    def isPalindrome(self, s: str) -> bool:
+        #  Turn into lowercase.
+        s = s.lower()
+        #  Only leave numbers and letters.
+        s = [s[i] for i in range(len(s)) if s[i].isalnum()]
+        #  Compare.
+        return s[::-1] == s
+
+
 if __name__ == "__main__":
     S = Solution()
     #  1137 (easy)
     print(S.tribonacci(25))
+    #  125 (easy)
+    print(S.isPalindrome(" "))
+    print(S.isPalindrome("race a car"))
+    print(S.isPalindrome("A man, a plan, a canal: Panama"))
 
 
