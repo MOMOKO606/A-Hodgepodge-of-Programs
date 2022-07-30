cd0c7b309ca72320b750024385debe89d0c7e4b7
BianLong0618 <bianlong0618@qq.com>
2022-07-30 00:31:56 -0400

    Update the solution for the Leetcode 105.

--- practice.py
+++ practice.py
@@ -554,15 +554,11 @@ class Solution:
 
     #  105(medium)
     def buildTree105(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder: return None
-        if len(preorder) == 1: return TreeNode(preorder[0])
-
+        if not preorder and not inorder: return None
         root = TreeNode(preorder[0])
         pos = inorder.index(preorder[0])
-
-        root.left = self.buildTree105(preorder[1:pos + 1], inorder[:pos])
-        root.right = self.buildTree105(preorder[pos + 1:], inorder[pos + 1:])
-
+        root.left = self.buildTree(preorder[1:pos + 1], inorder[:pos])
+        root.right = self.buildTree(preorder[pos + 1:], inorder[pos + 1:])
         return root
 
     #  106(medium)
