02dc1e7aa6dddbdab511e4bbd61187e525e5324c
BianLong0618 <bianlong0618@qq.com>
2022-08-03 14:06:53 -0400

    Update a iterative solution for the Leetcode 17.

--- practice.py
+++ practice.py
@@ -754,17 +754,14 @@ class Solution:
 
     #  17(medium)
     def letterCombinations(self, digits: str) -> List[str]:
-        def backtracking(digits, seq):
-            if not digits:
-                ans.append(seq)
-                return
-            for char in hashmap[digits[0]]:
-                backtracking(digits[1:], seq + char)
-
         hashmap = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
-        ans = []
-        backtracking(digits, "")
+        ans = [""]
+        for num in digits:
+            ans = [item + char for item in ans for char in hashmap[num]]
         return ans if digits else digits
+
+
+
 
 
 
