cc44782e7018f3a277b9f19b44d78b31903b8c35
BianLong0618 <bianlong0618@qq.com>
2022-08-05 15:31:34 -0400

    Update a solution for the Leetcode 50.

--- practice.py
+++ practice.py
@@ -746,7 +746,7 @@ class Solution:
         if n < 0: return 1 / self.myPow(x, -n)
         if n % 2:
             return x * self.myPow(x, n - 1)
-        else: return self.myPow(x ** 2, n // 2)
+        else: return self.myPow(x * x, n // 2)
 
     #  169(easy)
     def majorityElement(self, nums:List[int]) -> int:
