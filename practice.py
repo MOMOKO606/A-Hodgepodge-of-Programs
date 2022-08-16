5b9ce70368d055f478c789e854f4d866fbe82272
BianLong0618 <bianlong0618@qq.com>
2022-08-16 16:31:29 -0400

    Update a binary-search solution for the Leetcode 153.

--- practice.py
+++ practice.py
@@ -1079,10 +1079,24 @@ class Solution:
         low, high = 0, len(nums) - 1
         while low <= high:
             mid = (low + high) // 2
-            if nums[mid] == target: return True
-            elif target < nums[mid]: high = mid - 1
-            else: low = mid + 1
+            if nums[mid] == target:
+                return True
+            elif target < nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
         return False
+
+    #  153(medium)
+    def findMin(self, nums: List[int]) -> int:
+        low, high = 0, len(nums) - 1
+        while low < high:
+            mid = (low + high) // 2
+            if nums[low] <= nums[mid] < nums[high]: break
+            elif nums[low] <= nums[mid] and nums[mid] > nums[high]: low = mid + 1
+            else: high = mid
+        return nums[low]
+
 
 if __name__ == "__main__":
     S = Solution()
@@ -1356,7 +1370,12 @@ if __name__ == "__main__":
     print(S.isPerfectSquare(14))
 
     #  74(medium)
-    print("------------------------------------------------------------------------")
     print(S.searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3))
     print(S.searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13))
+
+    #  153(medium)
+    print("------------------------------------------------------------------------")
+    print(S.findMin([3, 4, 5, 1, 2]))
+    print(S.findMin([4, 5, 6, 7, 0, 1, 2]))
+    print(S.findMin([11, 13, 15, 17]))
     print("------------------------------------------------------------------------")
