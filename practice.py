1a3384d004506e1e43d72bb52e31243ff1858cdd
BianLong0618 <bianlong0618@qq.com>
2022-10-14 10:14:55 -0400

    Update another solution using dp and bit operations for the Leetcode 338. But I like the first solution since it's more clear and easy-understanding.

--- practice.py
+++ practice.py
@@ -1873,7 +1873,7 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = [0] * (n + 1)
         for i in range(1, n + 1):
-            ans[i] = ans[i & (i - 1)] + 1
+            ans[i] = ans[i >> 1] + (i & 1)
         return ans
 
 
